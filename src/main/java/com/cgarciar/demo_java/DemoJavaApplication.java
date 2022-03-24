c49b63787526522243a56fd57df97c12aeab2f4c
cgarciar <cgarciar@pichincha.com>
2022-03-23 20:40:09 -0500

    codigo nuevo a rama

---
+++ src/main/java/com/cgarciar/demo_java/DemoJavaApplication.java
@@ -0,0 +1,13 @@
+package com.cgarciar.demo_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cgarciar/demo_java/DemoJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cgarciar.demo_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
