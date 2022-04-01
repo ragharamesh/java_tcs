9a95e35c8b264017c33b7f5d5dd6d2f194de2c1d
lrvilla <yogolrvb@outlook.com>
2022-03-31 22:38:01 -0500

    publicacion inicial

---
+++ src/test/java/com/evaluacion/ejemplo/EjemploApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evaluacion.ejemplo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjemploApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
